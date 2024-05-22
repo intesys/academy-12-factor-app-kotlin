5891c982accb26f745ad14b4257d5faf6d8169ee
Enrico Costanzi <enrico.costanzi@intesys.it>
2024-05-22 14:27:49 +0200

    configs RIGHT!

--- src/main/kotlin/Configs.kt
+++ src/main/kotlin/Configs.kt
@@ -1,38 +1,57 @@
 package org.example
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.*
+
 class Configs {
 
-    /**
-     * Test environment
-     */
-    val CUSTOM_MESSAGE_TEST: String = "Hello World Test!"
-    val PORT_TEST: Int = 8080
-    val API_KEY_TEST: String = "abc1234"
+    var properties: Properties = mergeProperties(
+        loadPropertyFile("classpath:application.properties"), // default dev properties
+        loadPropertyFile("classpath:application-local.properties"), // default personal properties (not versioned)
+        loadPropertyFile("application.properties"), // file system properties (in production)
+    )
 
-    /**
-     * Prod environment
-     */
-    val CUSTOM_MESSAGE_PROD: String = "Hello World Prod!"
-    val PORT_PROD: Int = 9090
-    val API_KEY_PROD: String = "abc1234"
-
-    fun getCustomMessage(): String {
-        val env = System.getenv("environment")
-        return if (env == "prod") {
-            CUSTOM_MESSAGE_PROD
+    fun loadPropertyFile(filePath: String): Properties {
+        val properties = Properties();
+        if (filePath.startsWith("classpath:")) {
+            // read properties from the non versioned application-local.properties
+            val localResource = Configs::class.java.classLoader
+                .getResourceAsStream(filePath.substring("classpath:".length))
+            if (localResource != null) {
+                properties.load(localResource)
+            }
         } else {
-            println("No environment test, fallback to test environments")
-            CUSTOM_MESSAGE_TEST
+            //read properties from file system
+            val fileSystemPropertiesPath = Path.of("config", "application.properties")
+            if (Files.exists(fileSystemPropertiesPath)) {
+                val config = Files.newInputStream(fileSystemPropertiesPath)
+                properties.load(config)
+            }
+        }
+
+        return properties;
+    }
+
+    fun mergeProperties(vararg propertiesList: Properties): Properties {
+        val mergedProperties = Properties()
+
+        propertiesList.forEach { properties ->
+            mergedProperties.putAll(properties)
+        }
+
+        return mergedProperties
+    }
+
+    fun getProperty(propertyName: String, default: String = "NONE") : String {
+        return when(val value = System.getenv(propertyName)) {
+            null -> properties.getProperty(propertyName, default)
+            else -> value;
         }
     }
 
-    fun getServerPort(): Int {
-        val env = System.getenv("environment")
-        return if (env == "prod") {
-            PORT_PROD
-        } else {
-            PORT_TEST
-        }
+    fun getIntegerProperty(propertyName: String, default: String = "0"): Int {
+        return Integer.parseInt(getProperty(propertyName, default));
     }
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,6 @@ fun main() {
     val appConfigs = Configs()
 
     val app = Javalin.create(/*config*/)
-        .get("/") { ctx -> ctx.result(appConfigs.getCustomMessage()) }
-        .start(appConfigs.getServerPort())
+        .get("/") { ctx -> ctx.result(appConfigs.getProperty("CUSTOM_MESSAGE")) }
+        .start(appConfigs.getIntegerProperty("PORT"))
 }
